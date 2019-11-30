f1c5fb7a0d936458ff28545a9ade306de901ae71
Gaël James <gael.james@aweurope.be>
2019-11-30 16:28:56 +0100

    Bugfix

--- plugin.py
+++ plugin.py
@@ -90,7 +90,7 @@ from DomoticzWrapper import \
     #     DomoticzImage as Image, \
     #     DomoticzImages as Images
 
-d = DomoticzWrapper(Domoticz, Settings, Parameters, Images)
+d = DomoticzWrapper(Domoticz, Settings, Parameters, _Devices, Images)
 
 def onStart():
     d.Debugging(DomoticzDebugLevel.ShowAll)  # (DL.ShowAll)
