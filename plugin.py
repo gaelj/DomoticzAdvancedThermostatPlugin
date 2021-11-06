67e5fbafbfe6afe0353e57996caa530b23944b2f
Gaël James <gaeljames@gmail.com>
2021-11-06 08:57:17 +0100

    🔇 Remove loggings

--- plugin.py
+++ plugin.py
@@ -366,7 +366,7 @@ def ApplySetPoints():
     global pluginDevices
 
     enabledValue = pluginDevices.disabledSwitch.Read()
-    z.WriteLog("enabledValue: " + str(enabledValue))
+    # z.WriteLog("enabledValue: " + str(enabledValue))
     enabledValue = int(enabledValue) if enabledValue is not None else None
 
     if enabledValue == 0:
@@ -376,9 +376,9 @@ def ApplySetPoints():
     room1PresenceValue = pluginDevices.room1PresenceSwitch.Read()
     room2PresenceValue = pluginDevices.room2PresenceSwitch.Read()
 
-    z.WriteLog("thermostatControlValue: " + str(thermostatControlValue))
-    z.WriteLog("room1PresenceValue: " + str(room1PresenceValue))
-    z.WriteLog("room2PresenceValue: " + str(room2PresenceValue))
+    # z.WriteLog("thermostatControlValue: " + str(thermostatControlValue))
+    # z.WriteLog("room1PresenceValue: " + str(room1PresenceValue))
+    # z.WriteLog("room2PresenceValue: " + str(room2PresenceValue))
 
     thermostatControlValue = ThermostatControlValues(thermostatControlValue) if thermostatControlValue is not None else None
     room1PresenceValue = PresenceValues(room1PresenceValue) if room1PresenceValue is not None else None
